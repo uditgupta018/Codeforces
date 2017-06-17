ae1d93fd2f2f7e22a4cc2150ac458b49ec8699fa
ADOBENET\ugupta <uditgupta018@gmail.com>
2017-06-17 20:37:01 +0530

    creating,updating,querying Segment Tree code

--- Div 2C Problems/597C Subsequences.cpp
+++ Div 2C Problems/597C Subsequences.cpp
@@ -25,10 +25,45 @@
 //#define s second
 #define cf I64
 using namespace std;
-
+//Creating Segment Tree :-
+int tree[500000][12];
+void build_segment_tree(int tree[],int node,int a,int b,int len){
+	if(a>b) return ;
+	if(a==b){
+		for(int i=1;i<=len;i++){
+			tree[node][i]=0;
+		}
+	}
+	build_segment_tree(tree,2*node,a,(a+b)/2,len);
+	build_segment_tree(tree,2*node,(a+b)/2+1,b,len);
+	for(i=1;i<=len;i++){
+		tree[node][i]=tree[2*node][i]+tree[2*node+1][i];
+	}
+	return ;
+}
+void update_segment_tree(int tree[],int node,int a,int b,int start,int end,int len,int val){
+	if(a>b) return ;
+	//Current segment not in range
+	if(b<start||a>end) return ;
+	if(a>=start&&b<=end){
+		tree[node][len]+=val;
+		return ;
+	}
+	update_segment_tree(tree,2*node,a,(a+b)/2,start,end,len,val);
+	update_segment_tree(tree,2*node+1,(a+b)/2+1,b,start,end,len,val);
+	return ;
+}
+int query_segment_tree(int tree[],int node,int a,int b,int start,int end,int len){
+	//current segment not in range
+	if(a>b||b<start||a>end) return 0;
+	if(a>=start&&b<=end) return tree[node][len];
+	ans=query_segment_tree(tree,2*node,a,(a+b)/2,start,end,len)+query_segment_tree(tree,2*node+1,(a+b)/2+1,b,start,end,len);
+	return ans;
+}
 int main(){
-	int n,k;
+	int n,len;
 	//standard input
+	cin>>n>>len;
 	for(i=1;i<=n;i++){
 		cin>>arr[i];
 	}
@@ -53,6 +88,7 @@ int main(){
 		//             : we use tree[2] to store number of subsequences which end at value 2
 		//But we want number of subsequences of length 1 , 2 ,3 .. (k+1)
 		// So we will use 2-d Segment Tree
+		
 		
 
 	}
